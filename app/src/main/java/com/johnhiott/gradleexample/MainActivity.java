25ddf743449529395a4105373cdbc33e0efdc53f
John Hiott <john.hiott@passportparking.com>
2015-05-06 11:34:09 -0400

    setup gradle file and show variable values in main activity

--- app/src/main/java/com/johnhiott/gradleexample/MainActivity.java
+++ app/src/main/java/com/johnhiott/gradleexample/MainActivity.java
@@ -4,14 +4,30 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    @InjectView(R.id.bugReports) TextView mBugReports;
+    @InjectView(R.id.apiKey) TextView mApiKeyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        if (BuildConfig.SEND_BUG_REPORTS) {
+            mBugReports.setText(getString(R.string.bug_reports_enabled));
+        }else {
+            mBugReports.setText(getString(R.string.bug_reports_disabled));
+        }
+
+        mApiKeyTextView.setText(BuildConfig.DEV_API);
     }
 
 
